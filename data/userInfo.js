9fe9cd328ad8d217a33e60d2fe427d94094ee37a
each-Li <15290604023@163.com>
2016-12-17 18:01:05 +0800

    resume

---
+++ data/userInfo.js
@@ -0,0 +1,37 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "李宜其" , 
+    userPortrait:"images/icon/headerImg.jpg",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "1318245851",
+    userEmail:   "15290604023@163.com",
+    motto:  "躺下就永远不会再跌倒，可谁让我们都喜欢站着", 
+    userPhone: 18810609069,
+    userOrignPlace:  "河南省周口市太康县" ,
+    userSeatPlace:  "北京市",
+//  userWeibo: "javascript@miaov.com",
+    userAssessment: "富有探索精神，精通各种JS技巧与实现，为人和善。",   
+    userHobby: ["运动","游戏","电影"],
+//  userAward: ["最佳前端架构奖"]
+}
+
