492200f0ac2dddafdf76e2528ddb99af18665342
eachLee <eachli@163.com>
2018-06-22 17:33:21 +0800

    增加了几个自己的项目

--- data/project.js
+++ data/project.js
@@ -6,29 +6,56 @@
 	*       startTime: @value String              *         项目开始时间  格式为：2016.10.09
 	*       endTime: @value String 			      *         项目结束时间  格式为：2016.10.09
 	*       projectExplain: @value String         *         项目说明
-	*       projectLabel: @value Array            *         项目技术标签 
+	*       projectLabel: @value Array            *         项目技术标签
 			projectThumbnail: @value String       *         缩略图地址
-	*       
+	*
 	*   }
 */
 
 var projects = [
-		// {
-		// 	projectName:"整屏切换+动画制作“TweenMax库”",
-		// 	projectWebsite:"http://2013.miaov.com/miaov_demo/diagnosite/",
-		//     startTime:"2016.01.01",
-		// 	endTime:"至今",
-		// 	projectExplain:"JS整站开发之：整屏切换+动画制作神器“TweenMax库” 导航条和首屏出场动画、3D翻转实现、整屏切换，页面动画计算滚动条滚动距离， 拖动滚动条触发页面动画，完善每一屏内的图片翻转，缩小浏览器，做布局转换。",
-		// 	projectLabel:["html","css3","css","js"],
-		// 	projectThumbnail:"images/icon/1.png"
-		// },
-		// {
-		// 	projectName:"妙味VIP会员感恩墙",
-		// 	projectWebsite:"http://v.miaov.com/thanks/",
-		//     startTime:"2016.01.01",
-		// 	endTime:"至今",
-		// 	projectExplain:"用户观看视频的主页面，其大部分内容是通过JS加载的。为了提高性能，采用了延迟加载、延迟渲染、事件代理等优化方式。此外，还通过监听页面的window.onerror事件对异常信息进行采集，以便及时发现和排查故障。",
-		// 	projectLabel:["html","css3","css","js"],
-		// 	projectThumbnail:"images/icon/project-2.png"
-		// }
+	{
+		projectName: '微云网页版',
+		projectWebsite: 'https://eachlee.github.io/weiyun/',
+		startTime: '2016.05.06',
+		endTime: '至今',
+		projectExplain: 'JS整站开发之：大量的数据操作，和根据数据生成Dom，和数据的增删改查，包括文件夹修改 新建 重命名和移动....',
+		projectLabel: ['html', 'css3', 'css', 'js'],
+		projectThumbnail: 'images/icon/1.png'
+	},
+	{
+		projectName: '百度浏览器7.0宣传页',
+		projectWebsite: 'https://eachlee.github.io/baidu-browser/',
+		startTime: '2016.05.06',
+		endTime: '至今',
+		projectExplain: '整屏滑动和一些css3动画的展现',
+		projectLabel: ['html', 'css3', 'css', 'js'],
+		projectThumbnail: 'images/icon/project-2.png'
+	},
+	{
+		projectName: 'JQuery-web',
+		projectWebsite: 'https://eachlee.github.io/jQuery-web/',
+		startTime: '2016.05.06',
+		endTime: '至今',
+		projectExplain: '响应式设计 根据屏幕尺寸不同调整页面布局',
+		projectLabel: ['html', 'css3', 'css', 'js'],
+		projectThumbnail: 'images/icon/project-3.png'
+	},
+	{
+		projectName: 'UEhtml 设计网站',
+		projectWebsite: 'https://eachlee.github.io/UEhtml/',
+		startTime: '2016.05.06',
+		endTime: '至今',
+		projectExplain: '响应式设计 根据屏幕尺寸不同调整页面布局，以及一些css简单动画的展现',
+		projectLabel: ['html', 'css3', 'css', 'js'],
+		projectThumbnail: 'images/icon/project-4.png'
+	},
+	{
+		projectName: '百度移动端首页',
+		projectWebsite: 'https://eachlee.github.io/Mbaidu/',
+		startTime: '2016.05.06',
+		endTime: '至今',
+		projectExplain: '移动端Rem布局',
+		projectLabel: ['html', 'css3', 'css', 'js'],
+		projectThumbnail: 'images/icon/project-5.png'
+	}
 ]
